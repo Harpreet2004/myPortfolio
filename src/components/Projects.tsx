0f0399cc0e1f7da97f8fa2574a6559d8d1619278
harpreet2004 <harpreetsingh5066@gmail.com>
2025-01-10 19:35:47 +0530

    changes in skills, project & contact sections, added basic structure

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,55 @@ import Link from "next/link";
 
 export function ThreeDCardDemo() {
   return (
-    <CardContainer className="inter-var">
+    <>
+      <div className="h-[80rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center flex-col">
+         <div>My Projects</div>
+         <div className="grid grid-cols-3">
+         <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Make things float in air
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Hover over this card to unleash the power of CSS perspective
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src="../../public/../vercel.svg"
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-contain rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Try now →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            Sign up
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+         <CardContainer className="inter-var">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
@@ -51,5 +99,140 @@ export function ThreeDCardDemo() {
         </div>
       </CardBody>
     </CardContainer>
+         <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Make things float in air
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Hover over this card to unleash the power of CSS perspective
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src="../../public/../vercel.svg"
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Try now →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            Sign up
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+         <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Make things float in air
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Hover over this card to unleash the power of CSS perspective
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src="../../public/../vercel.svg"
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Try now →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            Sign up
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+         <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Make things float in air
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Hover over this card to unleash the power of CSS perspective
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src="../../public/../vercel.svg"
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Try now →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            Sign up
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+         </div>
+      </div>
+    </>
   );
 }
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,8 +5,38 @@ export function GlareCardDemo() {
     <>
       <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center flex-col">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <h2 className="text-white">Technical Skills 👨‍💻</h2>
         <div>
+        <h2 className="text-white">Technical Skills 👨‍💻</h2>
+        </div>
+        <div className="grid grid-cols-9">
+        <GlareCard className="h-[100px] flex flex-col items-center justify-center">
+      <img className="h-20 mt-20" src="../../public/../assets/react-svg.svg" alt="ww" />
+      <p className="text-white font-bold text-xl mt-4">ReactJs</p>
+        </GlareCard>
+        <GlareCard className="h-[100px] flex flex-col items-center justify-center">
+      <img className="h-20 mt-20" src="../../public/../assets/react-svg.svg" alt="ww" />
+      <p className="text-white font-bold text-xl mt-4">ReactJs</p>
+        </GlareCard>
+        <GlareCard className="h-[100px] flex flex-col items-center justify-center">
+      <img className="h-20 mt-20" src="../../public/../assets/react-svg.svg" alt="ww" />
+      <p className="text-white font-bold text-xl mt-4">ReactJs</p>
+        </GlareCard>
+        <GlareCard className="h-[100px] flex flex-col items-center justify-center">
+      <img className="h-20 mt-20" src="../../public/../assets/react-svg.svg" alt="ww" />
+      <p className="text-white font-bold text-xl mt-4">ReactJs</p>
+        </GlareCard>
+        <GlareCard className="h-[100px] flex flex-col items-center justify-center">
+      <img className="h-20 mt-20" src="../../public/../assets/react-svg.svg" alt="ww" />
+      <p className="text-white font-bold text-xl mt-4">ReactJs</p>
+        </GlareCard>
+        <GlareCard className="h-[100px] flex flex-col items-center justify-center">
+      <img className="h-20 mt-20" src="../../public/../assets/react-svg.svg" alt="ww" />
+      <p className="text-white font-bold text-xl mt-4">ReactJs</p>
+        </GlareCard>
+        <GlareCard className="h-[100px] flex flex-col items-center justify-center">
+      <img className="h-20 mt-20" src="../../public/../assets/react-svg.svg" alt="ww" />
+      <p className="text-white font-bold text-xl mt-4">ReactJs</p>
+        </GlareCard>
         <GlareCard className="h-[100px] flex flex-col items-center justify-center">
       <img className="h-20 mt-20" src="../../public/../assets/react-svg.svg" alt="ww" />
       <p className="text-white font-bold text-xl mt-4">ReactJs</p>
